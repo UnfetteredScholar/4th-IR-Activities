d2fb4e0a08b40af61cfcfd968e9211fea35ab7d6
Ato Toffah <atotoffah@gmail.com>
2022-10-12 15:27:40 +0000

    Add project files.

---
+++ 4thIR.TextSummarizeF/_4thIR.TextSummarizeF.Activities.Design/Designers/TextSummarizationDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.TextSummarizeF.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for TextSummarizationDesigner.xaml
+    /// </summary>
+    public partial class TextSummarizationDesigner
+    {
+        public TextSummarizationDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

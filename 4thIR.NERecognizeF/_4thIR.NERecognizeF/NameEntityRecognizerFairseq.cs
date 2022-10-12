4750d4588acd5a32f5d81f47c4d619ed00bf46da
Ato Toffah <atotoffah@gmail.com>
2022-10-12 13:46:16 +0000

    Add project files.

---
+++ 4thIR.NERecognizeF/_4thIR.NERecognizeF/NameEntityRecognizerFairseq.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+
+namespace NameEntityRecognitionFairseq
+{
+    public class WordTag
+    {
+        public WordTag()
+        {
+
+        }
+
+        public string text { get; set; }
+        public string value { get; set; }
+    }
+    public class NameEntityRecognizerFairseq
+    {
+        private class RequestContent
+        {
+            public RequestContent(string sentence)
+            {
+                this.sentence = sentence;
+            }
+
+            public string sentence { get; set; }
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public NameEntityRecognizerFairseq()
+        {
+            client.BaseAddress = new Uri("https://text-name-entity-recognition-flair-2.ai-sandbox.4th-ir.io");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<WordTag[]> RecognizeNameEntity(string sentence)
+        {
+            RequestContent requestContent = new RequestContent(sentence);
+
+            StringContent stringContent = new StringContent(JsonConvert.SerializeObject(requestContent), Encoding.UTF8, "application/json");
+
+            string requestUri = "/api/v1/sentence";
+            var response = await client.PostAsync(requestUri, stringContent);
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+
+                string r = await response.Content.ReadAsStringAsync();
+
+                WordTag[] result = JsonConvert.DeserializeObject<WordTag[]>(r);
+
+                return result;
+            }
+            catch (HttpRequestException ex)
+            {
+                string message = "";
+
+                if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    message = "string too long";
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                {
+                    message = "ML model not found";
+                }
+                else
+                {
+                    message = "Error: Unable to complete operation";
+                }
+
+                throw new Exception(message, ex);
+            }
+        }
+
+    }
+}

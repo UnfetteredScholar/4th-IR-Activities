3d3f8cf5c6d85a6d74d8064ba148663e67191edc
Ato Toffah <atotoffah@gmail.com>
2022-10-23 21:17:39 +0000

    Add project files.

---
+++ 4thIR.ImageClassifyMX/_4thIR.ImageClassifyMX/ClassificationLabel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ImageClassification
+{
+    /// <summary>
+    /// Contains a classification label and its probability/ accuracy
+    /// </summary>
+    public class ClassificationLabel
+    {
+        /// <summary>
+        /// Creates a new instance of the ClassificationLabel class
+        /// </summary>
+        public ClassificationLabel()
+        {
+
+        }
+
+        public string label { get; set; }
+        public double probability { get; set; }
+    }
+}

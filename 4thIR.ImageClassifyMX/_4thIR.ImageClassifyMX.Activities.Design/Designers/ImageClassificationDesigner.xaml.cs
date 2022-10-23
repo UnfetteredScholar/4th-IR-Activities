64191704dadbf9c020e3ced6bd8c229ac20db5fc
Ato Toffah <atotoffah@gmail.com>
2022-10-23 21:43:54 +0000

    Created activity

---
+++ 4thIR.ImageClassifyMX/_4thIR.ImageClassifyMX.Activities.Design/Designers/ImageClassificationDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.ImageClassifyMX.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ImageClassificationDesigner.xaml
+    /// </summary>
+    public partial class ImageClassificationDesigner
+    {
+        public ImageClassificationDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

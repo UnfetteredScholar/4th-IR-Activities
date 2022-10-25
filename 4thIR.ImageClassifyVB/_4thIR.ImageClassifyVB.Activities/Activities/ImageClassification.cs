2a3bd29f2a1d81409747e84d4b067d6e1cf0abca
Ato Toffah <atotoffah@gmail.com>
2022-10-25 20:18:03 +0000

    Removed unused reference

--- 4thIR.ImageClassifyVB/_4thIR.ImageClassifyVB.Activities/Activities/ImageClassification.cs
+++ 4thIR.ImageClassifyVB/_4thIR.ImageClassifyVB.Activities/Activities/ImageClassification.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using _4thIR.ImageClassifyVB.Activities.Properties;
 using UiPath.Shared.Activities;
 using UiPath.Shared.Activities.Localization;
-using Microsoft.Extensions.Http;
 using Microsoft.Extensions.DependencyInjection;
 using ImageClassification;
 using System.Net.Http;

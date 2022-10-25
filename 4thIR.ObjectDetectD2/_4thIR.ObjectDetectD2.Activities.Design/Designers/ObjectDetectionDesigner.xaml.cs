3a4a168e449e814e095a5d7828098e767045c7e1
Ato Toffah <atotoffah@gmail.com>
2022-10-25 23:31:26 +0000

    Created activity

---
+++ 4thIR.ObjectDetectD2/_4thIR.ObjectDetectD2.Activities.Design/Designers/ObjectDetectionDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.ObjectDetectD2.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ObjectDetectionDesigner.xaml
+    /// </summary>
+    public partial class ObjectDetectionDesigner
+    {
+        public ObjectDetectionDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

9bcd3ee729fd2d66462f7cd733e86ebda6f8e932
Ato Toffah <atotoffah@gmail.com>
2022-10-27 18:22:55 +0000

    Created activity

---
+++ 4thIR.PoseEstimateP/_4thIR.PoseEstimateP.Activities.Design/Designers/PoseEstimationDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.PoseEstimateP.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for PoseEstimationDesigner.xaml
+    /// </summary>
+    public partial class PoseEstimationDesigner
+    {
+        public PoseEstimationDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

87858f0f0504cbdb5591fae1fd3685379bbdff45
Ato Toffah <atotoffah@gmail.com>
2022-10-27 22:16:37 +0000

    Added classes

---
+++ 4thIR.AgeClassGVB/_4thIR.AgeClassGVB/AgeClassification/GoogeVitBase/AgeProbabilityPair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4thIR.AgeClassGVB.AgeClassification.GoogeVitBase
+{
+    public class AgeProbabilityPair
+    {
+        public AgeProbabilityPair(string ageRange, double probability)
+        {
+            AgeRange = ageRange;
+            Probability = probability;
+
+        }
+
+        public string AgeRange { get; set; }
+        public double Probability { get; set; }
+
+    }
+}

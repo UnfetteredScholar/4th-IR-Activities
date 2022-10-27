73c7b699baa39d247424672688add473a053275a
Ato Toffah <atotoffah@gmail.com>
2022-10-27 21:40:38 +0000

    Created activity

---
+++ 4thIR.SSegmentS/_4thIR.SSegmentS.Activities.Design/Designers/SemanticSegmentationDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.SSegmentS.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for SemanticSegmentationDesigner.xaml
+    /// </summary>
+    public partial class SemanticSegmentationDesigner
+    {
+        public SemanticSegmentationDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

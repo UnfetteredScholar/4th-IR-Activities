9edc9e08678a6df48c06fd0789a099af6909de95
Ato Toffah <atotoffah@gmail.com>
2022-10-27 22:16:51 +0000

    Created activity

---
+++ 4thIR.AgeClassGVB/_4thIR.AgeClassGVB.Activities.Design/Designers/AgeClassificationDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.AgeClassGVB.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for AgeClassificationDesigner.xaml
+    /// </summary>
+    public partial class AgeClassificationDesigner
+    {
+        public AgeClassificationDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

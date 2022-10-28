8074622aba47cb16cab7a0e564e5abc26e2a7cbe
Ato Toffah <atotoffah@gmail.com>
2022-10-28 23:43:21 +0000

    Created activity

---
+++ 4thIR.TextGenerateOG/_4thIR.TextGenerateOG.Activities.Design/DesignerMetadata.cs
@@ -0,0 +1,26 @@
+using System.Activities.Presentation.Metadata;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using _4thIR.TextGenerateOG.Activities.Design.Designers;
+using _4thIR.TextGenerateOG.Activities.Design.Properties;
+
+namespace _4thIR.TextGenerateOG.Activities.Design
+{
+    public class DesignerMetadata : IRegisterMetadata
+    {
+        public void Register()
+        {
+            var builder = new AttributeTableBuilder();
+            builder.ValidateTable();
+
+            var categoryAttribute = new CategoryAttribute($"{Resources.Category}");
+
+            builder.AddCustomAttributes(typeof(TextGeneration), categoryAttribute);
+            builder.AddCustomAttributes(typeof(TextGeneration), new DesignerAttribute(typeof(TextGenerationDesigner)));
+            builder.AddCustomAttributes(typeof(TextGeneration), new HelpKeywordAttribute(""));
+
+
+            MetadataStore.AddAttributeTable(builder.CreateTable());
+        }
+    }
+}

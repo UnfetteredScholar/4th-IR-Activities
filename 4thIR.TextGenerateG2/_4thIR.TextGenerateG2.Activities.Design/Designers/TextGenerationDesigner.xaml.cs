e9f1f9930be97081ceb208e87b8c8f1d504253a1
Ato Toffah <atotoffah@gmail.com>
2022-10-28 23:06:50 +0000

    Created activity

---
+++ 4thIR.TextGenerateG2/_4thIR.TextGenerateG2.Activities.Design/Designers/TextGenerationDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.TextGenerateG2.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for TextGenerationDesigner.xaml
+    /// </summary>
+    public partial class TextGenerationDesigner
+    {
+        public TextGenerationDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

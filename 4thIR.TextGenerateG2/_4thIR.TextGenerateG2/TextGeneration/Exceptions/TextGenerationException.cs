97710ba60faee1ca72eaf219c020cf82989b3b2a
Ato Toffah <atotoffah@gmail.com>
2022-10-28 22:58:12 +0000

    Added request classes

---
+++ 4thIR.TextGenerateG2/_4thIR.TextGenerateG2/TextGeneration/Exceptions/TextGenerationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4thIR.TextGenerateG2.TextGeneration.Exceptions
+{
+    public class TextGenerationException : Exception
+    {
+        public TextGenerationException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

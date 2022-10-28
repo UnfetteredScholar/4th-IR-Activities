f1796ff8f9c9ec113790d82b1598a287c4fbcf2d
Ato Toffah <atotoffah@gmail.com>
2022-10-28 22:27:54 +0000

    Added request classes

---
+++ 4thIR.QAnswerB/_4thIR.QAnswerB/QuestionAnswering/Exceptions/QuestionAnsweringException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace _4thIR.QAnswerB.QuestionAnswering.Exceptions
+{
+    public class QuestionAnsweringException : Exception
+    {
+        public QuestionAnsweringException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

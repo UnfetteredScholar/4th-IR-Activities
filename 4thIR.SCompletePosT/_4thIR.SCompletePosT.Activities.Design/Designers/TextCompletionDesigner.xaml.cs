0c0e2b652bd306a04427195967aee260d933aaab
Ato Toffah <atotoffah@gmail.com>
2022-10-29 00:09:29 +0000

    Created activity

---
+++ 4thIR.SCompletePosT/_4thIR.SCompletePosT.Activities.Design/Designers/TextCompletionDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.SCompletePosT.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for TextCompletionDesigner.xaml
+    /// </summary>
+    public partial class TextCompletionDesigner
+    {
+        public TextCompletionDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

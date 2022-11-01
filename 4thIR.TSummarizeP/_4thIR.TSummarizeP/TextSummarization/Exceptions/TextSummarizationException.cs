e6672a553a56d17bceded8aea5f5110bdef51bd3
Ato Toffah <atotoffah@gmail.com>
2022-11-01 16:18:26 +0000

    Added api request classes

---
+++ 4thIR.TSummarizeP/_4thIR.TSummarizeP/TextSummarization/Exceptions/TextSummarizationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4thIR.TSummarizeP.TextSummarization.Exceptions
+{
+    public class TextSummarizationException : Exception
+    {
+        public TextSummarizationException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

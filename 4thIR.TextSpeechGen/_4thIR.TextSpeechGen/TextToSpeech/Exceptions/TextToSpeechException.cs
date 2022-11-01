9b97a59271169e62d4033f6cd6a96d196ac14a26
Ato Toffah <atotoffah@gmail.com>
2022-11-01 18:53:15 +0000

    Added api request classes

---
+++ 4thIR.TextSpeechGen/_4thIR.TextSpeechGen/TextToSpeech/Exceptions/TextToSpeechException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4thIR.TextSpeechGen.TextToSpeech.Exceptions
+{
+    public class TextToSpeechException : Exception
+    {
+        public TextToSpeechException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

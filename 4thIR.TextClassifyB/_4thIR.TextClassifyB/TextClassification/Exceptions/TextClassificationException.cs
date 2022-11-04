8f920eccd5f2af647780e8392d559b9d17ef78d4
Ato Toffah <atotoffah@gmail.com>
2022-11-04 23:49:54 +0000

    Added API Request classes

---
+++ 4thIR.TextClassifyB/_4thIR.TextClassifyB/TextClassification/Exceptions/TextClassificationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4thIR.TextClassifyB.TextClassification.Exceptions
+{
+    public class TextClassificationException : Exception
+    {
+        public TextClassificationException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

6ead163e4cfaa212749b54c930d52debb0c597fd
Ato Toffah <atotoffah@gmail.com>
2022-11-04 23:00:41 +0000

    Added API Request classes

---
+++ 4thIR.DGenerate/_4thIR.DGenerate/TextGenerarion/Exceptions/DialogueGenerationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4thIR.DGenerate.TextGenerarion.Exceptions
+{
+    public class DialogueGenerationException : Exception
+    {
+        public DialogueGenerationException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

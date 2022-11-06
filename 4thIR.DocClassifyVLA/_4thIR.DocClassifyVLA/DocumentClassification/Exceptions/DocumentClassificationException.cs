1e3383a2558838205a331ec7fc56283c1601d753
Ato Toffah <atotoffah@gmail.com>
2022-11-06 11:51:49 +0000

    Added api request classes

---
+++ 4thIR.DocClassifyVLA/_4thIR.DocClassifyVLA/DocumentClassification/Exceptions/DocumentClassificationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4thIR.DocClassifyVLA.DocumentClassification.Exceptions
+{
+    public class DocumentClassificationException : Exception
+    {
+        public DocumentClassificationException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

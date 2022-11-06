abdf2a14d68421c2755e7da1caae1989f441144d
Ato Toffah <atotoffah@gmail.com>
2022-11-06 13:25:25 +0000

    Added API request classes

---
+++ 4thIR.TextUtility/_4thIR.TextUtility/TextUtilities/Exceptions/TextUtilityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4thIR.TextUtility.TextUtilities.Exceptions
+{
+    internal class TextUtilityException : Exception
+    {
+        public TextUtilityException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

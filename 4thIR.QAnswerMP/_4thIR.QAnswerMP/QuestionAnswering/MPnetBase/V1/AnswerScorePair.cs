29873f9ce215758b86c8d6b2cfe0937badabcc1d
Ato Toffah <atotoffah@gmail.com>
2022-11-14 22:23:22 +0000

    Added API Request Classes

---
+++ 4thIR.QAnswerMP/_4thIR.QAnswerMP/QuestionAnswering/MPnetBase/V1/AnswerScorePair.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace QuestionAnswering.MPnetBase.V1
+{
+    public class AnswerScorePair
+    {
+        public AnswerScorePair(string answer, double score)
+        {
+            Answer = answer;
+            Score = score;
+        }
+
+        public string Answer { get; set; }
+        public double Score { get; set; }
+    }
+}

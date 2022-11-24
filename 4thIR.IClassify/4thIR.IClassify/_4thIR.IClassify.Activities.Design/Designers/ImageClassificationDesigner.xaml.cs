a5deb2e914507c904d97756c60452460744d43e4
Ato Toffah <atotoffah@gmail.com>
2022-11-24 20:36:13 +0000

    Created Image Classifcation Deit Base Model activity

---
+++ 4thIR.IClassify/4thIR.IClassify/_4thIR.IClassify.Activities.Design/Designers/ImageClassificationDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace _4thIR.IClassify.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ImageClassificationDesigner.xaml
+    /// </summary>
+    public partial class ImageClassificationDesigner
+    {
+        public ImageClassificationDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}

15e0633089b130b83ac43379184247c9e8246685
Ato Toffah <atotoffah@gmail.com>
2022-12-03 22:59:34 +0000

    Created FourthIRActivity Abstract class

---
+++ 4thIR.ProcessScope/4thIR.ProcessScope/_4thIR.ProcessScope.Activities/Activities/FourthIRActivity.cs
@@ -0,0 +1,23 @@
+﻿using System.Activities;
+using UiPath.Shared.Activities;
+using _4thIR.ProcessScope.Activities.Properties;
+
+namespace _4thIR.ProcessScope.Activities.Activities
+{
+    public abstract class FourthIRActivity: ContinuableAsyncCodeActivity
+    {
+        protected FourthIRActivity()
+        {
+            Constraints.Add(ActivityConstraints.HasParentType<FourthIRActivity, ProcessScope>(string.Format(Resources.ValidationScope_Error, typeof(ProcessScope).Name)));
+        }
+    }
+
+    public abstract class FourthIRCodeActivity:CodeActivity
+    {
+        protected FourthIRCodeActivity()
+        {
+            Constraints.Add(ActivityConstraints.HasParentType<FourthIRCodeActivity, ProcessScope>(string.Format(Resources.ValidationScope_Error, typeof(ProcessScope).Name)));
+        }
+    }
+
+}

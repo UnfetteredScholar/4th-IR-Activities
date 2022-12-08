513ece3c137d53fd507e367da2991e761b78562c
Ato Toffah <atotoffah@gmail.com>
2022-12-08 19:30:02 +0000

    Sentence Transformer - MiniLM Base V2 (Sentence Similarity): Initial Project Version

---
+++ Sentence_Transformer_MiniLM_Base_V2_Sentence_Similarity/4thIR.TextParaphraseComparer/_4thIR.TextParaphraseComparer/TextComparison/Exceptions/TextComparisonException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextComparison.Exceptions
+{
+    public class TextComparisonException : Exception
+    {
+        public TextComparisonException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
